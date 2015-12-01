9a82e58fc6d93f2ab78d151f2b4663de55d2ccb3
Christopher Nascone <chris@nascone.net>
2015-12-01 12:27:13 -0500

    Initial definitions and tests

--- 01NotQuiteLisp.rkt
+++ 01NotQuiteLisp.rkt
@@ -10,3 +10,30 @@
 |#
 
 #lang racket
+(require rackunit)
+
+;; State
+(struct directions (list floor))
+
+;; Constants
+(define UP #\()
+(define DOWN #\))
+
+;; Definitions
+; Consumes a character and returns true if it means up, false otherwise
+(define (up? char)
+  (equal? char UP))
+; Consumes a character and returns true if it means down, false otherwise
+(define (down? char)
+  (equal? char DOWN))
+
+;; Tests
+; up?
+(check-eq? (up? #\() #t "Open parenthesis means up")
+(check-eq? (up? #\)) #f "Close paren means down")
+(check-eq? (up? #\5) #f "Non paren values should be false")
+; down?
+(check-eq? (down? #\)) #t "Close paren means down")
+(check-eq? (down? #\() #f "Open parenthesis means up")
+(check-eq? (down? #\5) #f "Non paren values should be false")
+
