b0ecf63cddd394e8376b50225f87755bee1a2f62
Christopher Nascone <chris@nascone.net>
2015-12-02 13:24:50 -0500

    Testing

--- 02NoMath/02NoMath.rkt
+++ 02NoMath/02NoMath.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require rackunit)
 #|
   To Use: Save your puzzle input as `input.txt` in the `02NoMath` folder, open
   the program in DrRacket, hit run.
@@ -6,6 +7,9 @@
 
 ;; State, a list of lines and a running total
 (struct state(lines total))
+
+;; Constants
+(define SEP "x")
 
 ;; Definitions
 ; Consumes the state of the world and returns true if no calculations are left
@@ -26,7 +30,7 @@
 
 ; Consumes a line and returns the square feet of wrapping paper it requires
 (define (calculate line)
-  (define lwh (map string->number (string-split line "x")))
+  (define lwh (map string->number (string-split line SEP)))
   (+ (surface-area lwh) (smallest-side lwh)))
 
 ; Consumes the world and returns the total square feet of wrapping paper required
@@ -44,3 +48,11 @@
   (total-sq-feet world))
 
 ;(start)
+
+;; Tests
+(test-case
+ "Testing 02NoMath.rkt"
+ (check-equal? (surface-area (list 2 3 4)) 52 "Surface area")
+ (check-equal? (surface-area (list 2 2 2)) 24 "Surface area")
+ (check-equal? (smallest-side (list 2 2 2)) 4 "Smallest side")
+ (check-equal? (smallest-side (list 4 2 8)) 8 "Smallest side, mixed order"))
