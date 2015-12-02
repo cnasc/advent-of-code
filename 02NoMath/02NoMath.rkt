79c0d449f69a00fee535cb1653f45f170a7b6e82
Christopher Nascone <chris@nascone.net>
2015-12-02 11:03:22 -0500

    Initial Commit

---
+++ 02NoMath/02NoMath.rkt
@@ -0,0 +1,4 @@
+#lang racket
+;; OK, so we want to use file->lines for this one, then parse the strings
+;; Have to figure out the path though.
+;(print (file->lines "Documents/advent-of-code/02NoMath/test.txt"))
