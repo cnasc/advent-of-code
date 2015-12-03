78b7cbf531f9bc572fc18380c081bee79095618f
Christopher Nascone <chris@nascone.net>
2015-12-03 15:38:23 -0500

    Make structs transparent

--- 03PerfectlySpherical/03PerfectlySpherical.rkt
+++ 03PerfectlySpherical/03PerfectlySpherical.rkt
@@ -5,10 +5,11 @@
 ;; DATA ;;
 ;;;;;;;;;;
 ; The state of the map, with directions to follow and actors
-(struct grid (directions santa robot))
+(struct grid (directions santa robot) #:transparent)
 
 ; The state of an actor, with stops made and current position
-(struct actor (stops posn))
+(struct actor (stops posn) #:transparent)
 
 ; Represents coordinates
-(struct posn (x y))
+(struct posn (x y) #:transparent)
+
