0335f3ffd74a431bf42974cfdf01405786a41819
Christopher Nascone <chris@nascone.net>
2015-12-03 16:08:48 -0500

    make stops, plus counter
    Santa makes the even moves (zero counts as even) robot makes the odd
    moves.

--- 03PerfectlySpherical/03PerfectlySpherical.rkt
+++ 03PerfectlySpherical/03PerfectlySpherical.rkt
@@ -4,8 +4,8 @@
 ;;;;;;;;;;
 ;; DATA ;;
 ;;;;;;;;;;
-; The state of the map, with directions to follow and actors
-(struct grid (directions santa robot) #:transparent)
+; The state of the map, with directions to follow, move counter, and actors
+(struct grid (directions counter santa robot) #:transparent)
 
 ; The state of an actor, with stops made and current position
 (struct actor (stops posn) #:transparent)
@@ -16,6 +16,11 @@
 ;;;;;;;;;;;;;;;;;;;;;
 ;; WORLD FUNCTIONS ;;
 ;;;;;;;;;;;;;;;;;;;;;
+; Consumes the grid and produces the grid after all stops are made
+(define (make-stops g)
+  (cond [(done? g) g]
+        [(even? (grid-counter g)) (make-stops (move-santa g))]
+        [else (make-stops (move-robot g))]))
 
 ;;;;;;;;;;
 ;; MAIN ;;
@@ -25,5 +30,5 @@
   (define start-point (posn 0 0))
   (define santa (actor (list start-point) start-point))
   (define robot (actor (list start-point) start-point))
-  (define g (grid directions santa robot))
+  (define g (grid directions 0 santa robot))
   (grid-directions (make-stops g)))
