ad035a7459ebcc2f765a2e0d561b4855a60f1ba7
Christopher Nascone <chris@nascone.net>
2015-12-03 15:37:09 -0500

    Data structures

--- 03PerfectlySpherical/03PerfectlySpherical.rkt
+++ 03PerfectlySpherical/03PerfectlySpherical.rkt
@@ -1,2 +1,14 @@
 #lang racket
 (require rackunit)
+
+;;;;;;;;;;
+;; DATA ;;
+;;;;;;;;;;
+; The state of the map, with directions to follow and actors
+(struct grid (directions santa robot))
+
+; The state of an actor, with stops made and current position
+(struct actor (stops posn))
+
+; Represents coordinates
+(struct posn (x y))
