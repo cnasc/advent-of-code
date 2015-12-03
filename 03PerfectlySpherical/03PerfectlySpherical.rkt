a344cd8444ca6dccbeee22fb4f60b59a72053cb1
Christopher Nascone <chris@nascone.net>
2015-12-03 16:53:05 -0500

    Data representation

--- 03PerfectlySpherical/03PerfectlySpherical.rkt
+++ 03PerfectlySpherical/03PerfectlySpherical.rkt
@@ -1,1 +1,9 @@
 #lang racket
+;;;;;;;;;;
+;; DATA ;;
+;;;;;;;;;;
+; Contains a list of directions and two lists of posns
+(struct grid (directions santa-posns robot-posns) #:transparent)
+
+; Represents 2D coordinates
+(struct posn (x y))
