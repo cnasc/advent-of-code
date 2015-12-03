df7c2b5d08cf6710ef68822fc2a7ea54fd1245e5
Christopher Nascone <chris@nascone.net>
2015-12-03 16:57:14 -0500

    Main and struct cleanup

--- 03PerfectlySpherical/03PerfectlySpherical.rkt
+++ 03PerfectlySpherical/03PerfectlySpherical.rkt
@@ -2,8 +2,18 @@
 ;;;;;;;;;;
 ;; DATA ;;
 ;;;;;;;;;;
-; Contains a list of directions and two lists of posns
-(struct grid (directions santa-posns robot-posns) #:transparent)
+; Contains a list of directions, a move counter, and two lists of posns
+(struct grid (directions counter santa-posns robot-posns) #:transparent)
 
 ; Represents 2D coordinates
-(struct posn (x y))
+(struct posn (x y) #:transparent)
+
+;;;;;;;;;;
+;; MAIN ;;
+;;;;;;;;;;
+(define (start)
+  (define directions (string->list (file->string "input.txt")))
+  (define santa (list (posn 0 0)))
+  (define robot (list (posn 0 0)))
+  (define g (grid directions 0 santa robot))
+  (length (grid-directions (make-stops g))))
